ecf4f086ba99416ecc99a16c6cb1bf861716d3a0
muhammadidrees <idrees@immersive-fox.com>
2024-08-29 00:24:45 +0500

    work on no found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div>
+      Page Not Found <p> Request Page Is Not Found</p>
+    </div>
+  );
+}
--- src/app/product/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,14 @@
+import { notFound } from "next/navigation";
 export default function ProductReviews({
   params,
 }: {
   params: { productId: number; reviewId: number };
 }) {
+  {
+    if (params?.reviewId > 200) {
+      return notFound();
+    }
+  }
   return (
     <>
       <h1>
