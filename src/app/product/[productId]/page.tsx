a384499a28466079230b6267f595ab7a1ccbc89c
muhammadidrees <idrees@immersive-fox.com>
2024-09-05 00:16:15 +0500

    add dynamic metadata

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,8 +1,17 @@
-export default function ProductDetail({
-  params,
-}: {
-  params: { productId: number };
-}) {
+import { Metadata } from "next";
+type Props = {
+  params: {
+    productId: number;
+  };
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product ${params?.productId}`,
+  };
+};
+
+export default function ProductDetail({ params }: Props) {
   return (
     <>
       <h1> Product Detail {params.productId}</h1>
