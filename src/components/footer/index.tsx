7b97093cea8dce72b4a7dd150b854e8f02ba4967
muhammadidrees <idrees@immersive-fox.com>
2024-09-04 23:52:00 +0500

    lean about layout

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ backgroundColor: "lightgray", padding: "1rem" }}>
+      <p> Footer </p>
+    </footer>
+  );
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header style={{ backgroundColor: "lightgray", padding: "1rem" }}>
+      <p> Header </p>
+    </header>
+  );
+}
