9491da940957020310ac73a493afcbeeb55d8f1a
MuhammadHaziq <muhammadhaziq341@gmail.com>
2024-09-30 23:56:41 +0500

    Add Links And App

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 export default function Product() {
   return (
     <>
       <h1>Product Page</h1>
-      <h4>Product 1</h4>
-      <h4>Product 2</h4>
-      <h4>Product 3</h4>
+      <h4><Link href={'/product/1'}>Product 1</Link></h4>
+      <h4><Link href={'/product/2'}>Product 2</Link></h4>
+      {/** Click On Link And After That Click Broswer back Buton And Check Replace Functionality */}
+      <h4><Link href={'/product/3'} replace>Product 3</Link></h4>
+      <h4><Link href={'/'}>Home</Link></h4>
     </>
   );
 }
