8b54cbcae0b885d432d494592b58547849d3d1d4
MuhammadHaziq <muhammadhaziq341@gmail.com>
2024-10-29 23:47:47 +0500

    add simple route for more folder

---
+++ src/app/intercepting-route/f1/f3/page.tsx
@@ -0,0 +1,5 @@
+import Card from "@/components/card";
+
+export default function F3(){
+    return <> <Card> F3 Page</Card></>
+}
---
+++ src/app/intercepting-route/f1/f4/page.tsx
@@ -0,0 +1,7 @@
+import Card from "@/components/card";
+import Link from "next/link";
+
+export default function F4(){
+    return <> <Card> F4 Page
+           <div> <Link href="/intercepting-route/f1/f3"> F3 Page</Link></div></Card></>
+}
