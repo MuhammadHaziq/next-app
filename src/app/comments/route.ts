dfd2ef3752d5cc55309823404a50d12df30b8314
MuhammadHaziq <muhammadhaziq341@gmail.com>
2024-10-31 00:09:40 +0500

    add comment post request

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -3,3 +3,18 @@ import { comments } from "./data";
 export async function GET() {
     return  Response.json(comments)
 }
+
+export async function POST(request:Request) {
+    const comment = await request.json();
+    comments.push({
+        id:comments?.length + 1,
+        text:comment?.text
+    })
+
+    return new Response(JSON.stringify(comments), {
+        headers:{
+            "Content-Type":"application/json"
+        },
+        status:201
+    })
+}
