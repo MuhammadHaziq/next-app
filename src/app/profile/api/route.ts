a93ceaafb7303d0918f55cfa0f50c68e46b4f7d2
MuhammadHaziq <muhammadhaziq341@gmail.com>
2024-11-07 23:30:23 +0500

    set response headers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,14 @@
-export async function GET(){
-    return new Response("Profile API Route Handler")
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request:NextRequest){
+    const headerList = headers()
+    console.log(headerList.get("Authorization"))
+    // return new Response("Profile API Route Handler")
+    /** set response header in response and check in browser network response */
+    return new Response("<h1>Profile API Route Handler<h1>", {
+        headers:{
+            "Content-type":"text/html"
+        }
+    })
 }
