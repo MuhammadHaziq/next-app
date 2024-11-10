df27991e326226d946687ef8bb82cc154cd14577
MuhammadHaziq <muhammadhaziq341@gmail.com>
2024-11-10 23:57:47 +0500

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function middleware(request:NextRequest){
+    /** Set Cookies And Headers */
+
+    /** check Difference by uncomment below line and comment NextResponse.redirect(new URL("/", request.url)); this*/
+    
+    // const response = NextResponse.next()
+    const response = NextResponse.redirect(new URL("/", request.url));
+    const theme = response.cookies.get("theme")
+    if(!theme){
+        response.cookies.set("theme", "dark")
+    }
+
+    response.headers.set("custom-header", "custom-value")
+    return response
+    // return NextResponse.redirect(new URL("/", request.url));
+}
+
+export const config = {
+    matcher:"/profile"
+}
