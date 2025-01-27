986330096e9c72b83ce9dd8a13cead51c2099f35
Muhammad Haziq <muhammadhaziq341@gmail.com>
2025-01-28 00:12:30 +0500

    create docker file and comment server code

--- server.ts
+++ server.ts
@@ -1,19 +1,21 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const route = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
+// import { Request, Response, NextFunction } from 'express'; // Import types for req, res, and next
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const route = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
 
-server.use((req, res, next) => {
-    console.log("Request Received")
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-})
-server.use(route);
+// server.use(middlewares);
 
-server.listen(3001, () => {
-    console.log('JSON Server is running')
-})
+// server.use((req: Request, res: Response, next: NextFunction) => {
+//     console.log("Request Received")
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     next();
+// })
+// server.use(route);
+
+// server.listen(3001, () => {
+//     console.log('JSON Server is running')
+// })
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,61 +1,64 @@
-/** Check After Uncomment this in JSON server Termianl
- *  When we add this line in start of server component then it will cache the data rather the
- *  no-store request
- *
- */
-// export const fetchCache = "default-cache";
+// /** Check After Uncomment this in JSON server Termianl
+//  *  When we add this line in start of server component then it will cache the data rather the
+//  *  no-store request
+//  *
+//  */
+// // export const fetchCache = "default-cache";
 
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 
-type Product = {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-};
+// type Product = {
+//   id: number;
+//   title: string;
+//   price: number;
+//   description: string;
+// };
 
-/**
- *  Data Fetch Cache
- * By data get api data cache in .next/cache folder and it will be used when we call the same api again
- * if we use no-store then it will not cache the data
- * if we use default-cache then it will cache the data execpt where we use cache:no-store
- * if we use custome function such as cookies, header, searchparems then it will not cache the data
- * API Call (it's cache by default)
- * Custom Function call
- * API Call (it's not cache due to above custome function)
- *
- */
+// /**
+//  *  Data Fetch Cache
+//  * By data get api data cache in .next/cache folder and it will be used when we call the same api again
+//  * if we use no-store then it will not cache the data
+//  * if we use default-cache then it will cache the data execpt where we use cache:no-store
+//  * if we use custome function such as cookies, header, searchparems then it will not cache the data
+//  * API Call (it's cache by default)
+//  * Custom Function call
+//  * API Call (it's not cache due to above custome function)
+//  *
+//  */
 
+// export default async function ProductPage() {
+//   /** This API Response is cached because it's called before no-store */
+//   // const responseDetail = await fetch("http://localhost:3001/products/1");
+//   const response = await fetch("http://localhost:3001/products", {
+//     /** If we set cache = no-store then fetch send request for data every time when user load the page
+//      * no-store un cache the all subsequent fetch requests
+//      * please verify by comment the cache and check in json server terminal
+//      */
+//     // cache: "no-store",
+//   });
+
+//   /** Comment This and see difference */
+//   const cookiesStore = cookies();
+//   cookiesStore.get("theme");
+
+//   /** This API Response is not cached because it's subsequent of no-store */
+//   const responseDetail1 = await fetch("http://localhost:3001/products/1");
+//   const products: Product[] = await response.json();
+//   return (
+//     <ul className="space-y-4 p-4">
+//       {products.map((product: Product) => (
+//         <li
+//           key={product.id}
+//           className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+//         >
+//           <h2 className="text-xl font-semibold">{product.title}</h2>
+//           <p>{product.description}</p>
+//           <p className="text-lg font-medium">${product.price}</p>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
 export default async function ProductPage() {
-  /** This API Response is cached because it's called before no-store */
-  // const responseDetail = await fetch("http://localhost:3001/products/1");
-  const response = await fetch("http://localhost:3001/products", {
-    /** If we set cache = no-store then fetch send request for data every time when user load the page
-     * no-store un cache the all subsequent fetch requests
-     * please verify by comment the cache and check in json server terminal
-     */
-    // cache: "no-store",
-  });
-
-  /** Comment This and see difference */
-  const cookiesStore = cookies();
-  cookiesStore.get("theme");
-
-  /** This API Response is not cached because it's subsequent of no-store */
-  const responseDetail1 = await fetch("http://localhost:3001/products/1");
-  const products: Product[] = await response.json();
-  return (
-    <ul className="space-y-4 p-4">
-      {products.map((product: Product) => (
-        <li
-          key={product.id}
-          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
-        >
-          <h2 className="text-xl font-semibold">{product.title}</h2>
-          <p>{product.description}</p>
-          <p className="text-lg font-medium">${product.price}</p>
-        </li>
-      ))}
-    </ul>
-  );
+  return <>Product Page JSON Server</>;
 }
